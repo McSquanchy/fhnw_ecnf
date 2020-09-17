6760b09b8090aa9c9a5a16ce32529bf3d87f0a68
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-17 18:29:59 +0200

    Added Solution PersonAdminSystem

---
+++ PersonAdminSystem/PersonAdminSystem/PersonAdminApplication.cs
@@ -0,0 +1,19 @@
+﻿using PersonAdminLib;
+using System;
+using System.Reflection;
+
+namespace PersonAdminSystem
+{
+    class PersonAdminApplication
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person("Hans", "Meier");
+            Person p2 = new Person("Fritz", "Loser");
+            Console.WriteLine($"Person 1: {p1.Firstname} {p1.Surname}");
+            Console.WriteLine($"Person 2: {p2.Firstname} {p2.Surname}");
+            Console.ReadKey();
+        }
+    }
+}
