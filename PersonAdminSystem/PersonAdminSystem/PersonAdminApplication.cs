da74294e161338b8699ab40e0aae30891d4aad9f
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-24 17:50:33 +0200

    Modified PersonAdminApplication.cs
    Changed main method

--- PersonAdminSystem/PersonAdminSystem/PersonAdminApplication.cs
+++ PersonAdminSystem/PersonAdminSystem/PersonAdminApplication.cs
@@ -1,6 +1,5 @@
-﻿using PersonAdminLib;
-using System;
-using System.Reflection;
+﻿using System;
+using PersonAdminLib;
 
 namespace PersonAdminSystem
 {
@@ -9,10 +8,16 @@ namespace PersonAdminSystem
         static void Main(string[] args)
         {
 
-            Person p1 = new Person("Hans", "Meier");
-            Person p2 = new Person("Fritz", "Loser");
-            Console.WriteLine($"Person 1: {p1.Firstname} {p1.Surname}");
-            Console.WriteLine($"Person 2: {p2.Firstname} {p2.Surname}");
+            //Person p1 = new Person("Hans", "Meier");
+            //Person p2 = new Person("Fritz", "Loser");
+            //Console.WriteLine($"Person 1: {p1.Firstname} {p1.Surname}");
+            //Console.WriteLine($"Person 2: {p2.Firstname} {p2.Surname}");
+            var personRegister = new PersonRegister();
+
+            Console.WriteLine("Person: {0} {1}",
+                personRegister[personRegister.Count - 1].Firstname,
+                personRegister[personRegister.Count - 1].Surname);
+            Console.WriteLine($"Person: {personRegister[0].Firstname} {personRegister[0].Surname}");
             Console.ReadKey();
         }
     }
