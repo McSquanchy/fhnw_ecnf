459e5bfda9f2c70eaca863ab4afac99c25a09048
Kevin Buman <kevin.buman@students.fhnw.ch>
2020-09-24 17:50:22 +0200

    Added PersonRegister.cs
    Added PersonRegister class

---
+++ PersonAdminSystem/PersonAdminLib/PersonRegister.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PersonAdminLib
+{
+    public class PersonRegister
+    {
+        private List<Person> personList;
+
+        public PersonRegister() 
+        {
+            personList = new List<Person>();
+            personList.Add(new Person("Hans", "Müller"));
+            personList.Add(new Person("Erich", "Weber"));
+            personList.Add(new Person("Jean", "Ziegler"));
+        }
+
+ 
+        public int Count { get { return personList.Count; } }
+
+        public Person this[int index]
+        {
+            get { return this.personList[index]; }
+        }
+    }
+}
